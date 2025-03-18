51295666b31731c10c6ef03a874483a4d410484f
Salah Eddine Hrimeche <shrimech@c2r3p10.1337.ma>
2025-03-18 07:18:33 +0000

    so_long

---
+++ get_next_line/get_next_line_bonus.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shrimech <shrimech@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/27 23:44:06 by shrimech          #+#    #+#             */
+/*   Updated: 2024/12/29 02:48:03 by shrimech         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+//# include <fcntl.h> // khesni n7aydha
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strdup(const char *str);
+
+#endif
---
+++ includes/so_long.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shrimech <shrimech@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/13 17:12:33 by shrimech          #+#    #+#             */
+/*   Updated: 2025/03/18 07:09:34 by shrimech         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include "../get_next_line/get_next_line.h"
+# include "../libft/libft.h"
+# include "/usr/include/minilibx-linux/mlx.h"
+
+
+
+#endif
+
