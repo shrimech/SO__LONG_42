6ea9a10922d40db0eb9d1a220ce093a94f54642f
Salah Eddine Hrimeche <shrimech@c2r3p10.1337.ma>
2025-03-22 09:20:01 +0000

    so_long

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: shrimech <shrimech@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 04:42:44 by shrimech          #+#    #+#             */
-/*   Updated: 2025/03/21 22:59:35 by shrimech         ###   ########.fr       */
+/*   Updated: 2025/03/22 06:45:04 by shrimech         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ size_t	ft_strlen(const char *str)
 	size_t	i;
 
 	i = 0;
-	while (str[i] && str[i] != '\n')
+	while (str[i] )
 		i++;
 	return (i);
 }
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: shrimech <shrimech@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 07:10:31 by shrimech          #+#    #+#             */
-/*   Updated: 2025/03/21 23:01:06 by shrimech         ###   ########.fr       */
+/*   Updated: 2025/03/22 09:15:34 by shrimech         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,71 @@ typedef struct s_game
     int width;
     int high;
     char **map;
+    int colect;
+    int exit;
+    int player;
 }t_game;
 
-int countnewline(int fd)
+int elements_count(t_game *game)
 {
-    
+    int i =0;
+    int j;
+    while (i < game->high)
+    {
+        j = 0;
+        while (j < game->width)
+        {
+            if (game->map[i][j] == 'C')
+                game->colect++;
+            if (game->map[i][j] == 'E')
+                game->exit++;
+            if (game->map[i][j] == 'P')
+                game->player++;
+            j++;
+        }
+        i++;
+    }
+    if(game->colect < 1 || game->exit != 1 || game->player != 1)
+        return(0);
+    return (1);
+}
+
+int map_elements(t_game *game)
+{
+    int i = 0;
+    int j;
+    while (game->map[i] != NULL)
+    {
+        j = 0; 
+        while(game->map[i][j] != '\0' && game->map[i][j] != '\n')
+        {
+            if (game->map[i][j] == '0' || game->map[i][j] == '1' ||game->map[i][j] == 'P' || game->map[i][j] == 'C' || game->map[i][j] == 'E')
+                j++;
+            else
+                return(0);
+        }
+        i++;
+    }
+    j = 0;
+    while((game->map[0][j] != '\0' && game->map[0][j] != '\n') || (game->map[game->high - 1][j] != '\0' && game->map[game->high - 1][j] != '\n'))
+    {
+        if (game->map[0][j] != '1' || game->map[game->high - 1][j] != '1')
+            return(0);
+        j++;
+    }
+    return(1);
+}
+
+size_t	gt_strlen(const char *str)
+{
+	size_t	i;
+
+    if (str == NULL)
+        return 0;
+	i = 0;
+	while (str[i] && str[i] != '\n')
+		i++;
+	return (i);
 }
 
 int check_map(char *av, t_game *game)
@@ -33,18 +93,26 @@ int check_map(char *av, t_game *game)
     fd = open(av , O_RDONLY);
     //game->map = malloc(countnewline(fd))
     char *line = get_next_line(fd);
-    game->width = ft_strlen(line);
+    game->width = gt_strlen(line);
     while(line)
     {
-        game->map[i] = malloc(ft_strlen(line)+12);
+        game->map[i] = malloc(ft_strlen(line));
         game->map[i]=line;
-        line = get_next_line(fd);
-        if(ft_strlen(line) != game->width || game->map[i][0] != '1')
+        if(gt_strlen(line) != game->width || game->map[i][0] != '1' || game->map[i][gt_strlen(line) - 1] != '1')
             return(0);
+        line = get_next_line(fd);
         i++;
     }
+    game->map[i] = NULL;
+    game->high = i;
     close(fd);
     return(1);
+}
+void game_init(t_game *game)
+{
+    game->colect = 0;
+    game->exit = 0;
+    game->player = 0;
 }
 
 #include<stdio.h>
@@ -56,10 +124,11 @@ int main(int ac,char **av)
     i = 0;
     if (ac == 2)
     {
-        if (check_map(av[1],game) == 0)
+        game_init(game);
+        if (check_map(av[1],game) == 0 || map_elements(game) == 0 || elements_count(game) == 0)
             return(write(1,"ERROR : invalid map\n",20),0);
-        printf("%s\n",game->map[5]);
+        printf("%d\n",game->high);
     }
     else
-        write(1,"Error: enter <./so_long> <mab.ber>",34);
+        write(1,"Error: enter <./so_long> <map.ber>",34);
 }
