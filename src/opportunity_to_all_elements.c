a272e4006037816f76d4ede831a1b7a0a75587b4
Salah Eddine Hrimeche <shrimech@c2r3p10.1337.ma>
2025-03-24 08:38:05 +0000

    so_long

---
+++ src/opportunity_to_all_elements.c
@@ -0,0 +1,110 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   opportunity_to_all_elements.c                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shrimech <shrimech@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/24 07:20:53 by shrimech          #+#    #+#             */
+/*   Updated: 2025/03/24 08:35:51 by shrimech         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"../includes/so_long.h"
+void	find_player_pos(t_game *game)
+{
+	int	x;
+	int	y;
+
+	x = 0;
+	game->p_x = 0;
+	game->p_y = 0;
+	while (x < game->high)
+	{
+		y = 0;
+		while (y < game->width)
+		{
+			if (game->map[x][y] == 'P')
+			{
+				game->p_y = y;
+				game->p_x = x;
+				return ;
+			}
+			y++;
+		}
+		x++;
+	}
+}
+
+void	flood_fill(char **map, int x, int y)
+{
+	if (map[x][y] == '1' || map[x][y] == 'X')
+		return ;
+	map[x][y] = 'X';
+	flood_fill(map, x + 1, y);
+	flood_fill(map, x - 1, y);
+	flood_fill(map, x, y + 1);
+	flood_fill(map, x, y - 1);
+}
+
+char	**duplicate_map(t_game *game)
+{
+	int		i;
+	char	**copy;
+
+	i = 0;
+	copy = malloc(sizeof(char *) * (game->width + 1));
+	if (!copy)
+		return (NULL);
+	while (game->map[i])
+	{
+		copy[i] = ft_strdup(game->map[i]);
+		if (!copy[i])
+		{
+			while (--i >= 0)
+				free(copy[i]);
+			free(copy);
+			return (NULL);
+		}
+		i++;
+	}
+	copy[i] = NULL;
+	return (copy);
+}
+
+int	check_oporunity_to_col_exit(t_game *game)
+{
+	int		i;
+	int		j;
+	int		c;
+	char	**coppy;
+
+	i = 0;
+	c = 0;
+	find_player_pos(game);
+	coppy = duplicate_map(game);
+	flood_fill(coppy, game->p_x, game->p_y);
+	while (i < game->high)
+	{
+		j = 0;
+		while (j < game->width)
+		{
+			if (coppy[i][j] == 'C' || coppy[i][j] == 'E')
+				c++;
+			j++;
+		}
+		i++;
+	}
+	if (c != 0)
+		return (0);
+	return (1);
+}
+
+void	game_init(t_game *game)
+{
+	game->colect = 0;
+	game->exit = 0;
+	game->player = 0;
+	game->high = 0;
+    game->map = NULL;
+}
---
+++ src/parcing.c
@@ -0,0 +1,119 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parcing.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shrimech <shrimech@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/24 07:26:05 by shrimech          #+#    #+#             */
+/*   Updated: 2025/03/24 08:22:15 by shrimech         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/so_long.h"
+
+int	elements_count(t_game *game)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < game->high)
+	{
+		j = 0;
+		while (j < game->width)
+		{
+			if (game->map[i][j] == 'C')
+				game->colect++;
+			if (game->map[i][j] == 'E')
+				game->exit++;
+			if (game->map[i][j] == 'P')
+				game->player++;
+			j++;
+		}
+		i++;
+	}
+	if (game->colect < 1 || game->exit != 1 || game->player != 1)
+		return (0);
+	return (1);
+}
+
+int	map_elements(t_game *game)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (game->map[i] != NULL)
+	{
+		j = 0;
+		while (game->map[i][j] != '\0' && game->map[i][j] != '\n')
+		{
+			if (game->map[i][j] == '0' || game->map[i][j] == '1'
+				|| game->map[i][j] == 'P' || game->map[i][j] == 'C'
+				|| game->map[i][j] == 'E')
+				j++;
+			else
+				return (0);
+		}
+		i++;
+	}
+	return(1);
+}
+
+size_t	gt_strlen(const char *str)
+{
+	size_t	i;
+
+	if (str == NULL)
+		return (0);
+	i = 0;
+	while (str[i] && str[i] != '\n')
+		i++;
+	return (i);
+}
+
+void	hight_map(t_game *game, char *filename)
+{
+	int		fd;
+	char	*line;
+
+	fd = open(filename, O_RDONLY);
+	line = get_next_line(fd);
+	while (line)
+	{
+		game->high++;
+		free(line);
+		line = get_next_line(fd);
+	}
+	free(line);
+	close(fd);
+}
+
+int	check_map(char *av, t_game *game)
+{
+	int		fd;
+	int		i;
+	char	*line;
+
+	i = 0;
+	fd = open(av, O_RDONLY);
+	game->map = malloc(sizeof(char *) * (game->high + 1));
+	line = get_next_line(fd);
+	if (!line)
+		return (0);
+	game->width = gt_strlen(line);
+	while (line)
+	{
+		game->map[i] = ft_strdup(line);
+		if (gt_strlen(line) != game->width || game->map[i][0] != '1'
+			|| game->map[i][gt_strlen(line) - 1] != '1')
+			return (0);
+		line = get_next_line(fd);
+		i++;
+	}
+	game->map[i] = NULL;
+	game->high = i;
+	close(fd);
+	return (1);
+}
